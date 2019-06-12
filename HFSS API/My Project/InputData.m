49d355fe8c55b9f20d30563411b334b703f96b2a
Mateo <44451446+MateoTito@users.noreply.github.com>
2019-06-11 22:59:20 -0500

    Committed

---
+++ HFSS API/My Project/InputData.m
@@ -0,0 +1,31 @@
+% Input variables for the DiscreteReflector and
+% FourPanelReflector files
+%% ***********************************************************************
+%                          4 Panel Generator
+%% ***********************************************************************
+% May 2019
+
+
+%% Operating frequency
+freq=20; % GHz
+
+%% Reflector Geometry
+W = 230; % mm
+Lp = 10; 
+Wp = 10;
+theta_deg = 23; % Panel tilt.
+% Conversion from deg to rad.
+theta = theta_deg*pi/180; % rad
+D = W/cos(theta); % Diameter in mm 
+centerX = W/2; % Center of the panel
+dy = 16;dx = 16 ;
+% Auxiliar variables.
+contY = 0;contX = 0; 
+% Focal point of the paraboloid x^2 + y^2 = 4*f*z.
+f = W/(2*tan(theta));
+% Frequency work.
+fr = 8.4e9; % Hz
+% Speed of ligth.
+c = 3e11; % m/s
+% Wave number in void.
+k0 = 2*pi*(fr/c); % 1/m
